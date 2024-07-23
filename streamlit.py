b574eb9356fac329d68ac6c3e66848710220b7e5
pjy <nihoo4u@gmail.com>
2024-07-23 16:22:05 +0900

    modify map

--- streamlit.py
+++ streamlit.py
@@ -1,1 +1,16 @@
-print("hi")
+ st.write('#괄호')
+ view=[100,150,30]
+ view
+ st.write('## barcart')
+ st.bar_chart(view)
+ sview= pd.Series(view)
+ st.write('## gogo')
+ data=pd.DataFrame({'index':['A','B','C'],
+                    'values':[10,20,30]})
+ st.write('## chart2!!')
+ st.bar_chart(data,x='index',y='values',use_container_width=True)
+ # m=folium.Map(location=[37.42637222,126.9898],zoom_start=16)
+ # folium.Marker([37.42637222,126.9898],
+ #               popup='junyeon',
+ #               tooltip='junyeon').add_to(m)
+ # st_data=st_folium(m,width=725)
