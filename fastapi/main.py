3bf14bcc090597dd1948bb1909fe4625f39f83d7
yoonsuk Lee <leyo2001@naver.com>
2024-07-31 17:21:31 +0900

    지도클릭구현+ 이후 백 통신 테스트

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,18 +1,22 @@
-from typing import Union
 import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
-
+my_host = "127.0.0.1"
+my_port = 8000
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/seoul")
+def read_seoul():
+    return "서버에서온 서울데이터"
+
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int):
+    return {"item_id": item_id}
 
 if __name__ == "__main__":
-    uvicorn.run(app,host="0.0.0.0",port=8000)
+    uvicorn.run(app,host=my_host,port=my_port)
---
+++ streamlit/pages/page1.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.title('페이지1')
