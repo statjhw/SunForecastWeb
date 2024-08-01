e746421e6ebef88d678220ec1c439a6c8c04bd8a
yoonsuk Lee <leyo2001@naver.com>
2024-08-01 17:15:04 +0900

    슬라이더

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,9 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
+from datetime import datetime
 
 app = FastAPI()
 my_host = "127.0.0.1"
 my_port = 8000
+
+
+#지역 리스트
+regions = ['seoul', 'incheon', 'kyunggi', 'gangwon', 'chungnam',
+            'chungbuk', 'daejeon', 'sejong', 'gyeongbuk', 'gyeongnam',
+            'ulsan', 'daegu', 'jeonbuk', 'jeonnam', 'kwangju', 'busan',
+            'jeju', 'ulleungdo_dokdo' ]
 
 @app.get("/")
 def read_root():
@@ -11,8 +19,16 @@ def read_root():
 
 
 @app.get("/seoul")
-def read_seoul():
-    return "서버에서온 서울데이터"
+def read_seoul(unit: str, start: datetime, end: datetime):
+
+    if(unit == '년'):
+        return f'서버에서 온 {start.year}~{end.year}의 년으로 된 서울데이터'
+    elif (unit == '월'):
+        return f'서버에서 온 {start:%Y-%m}~{end:%Y-%m}의 월으로 된 서울데이터'
+    else:
+        return f'서버에서 온 {start:%Y-%m-%d}~{end:%Y-%m-%d}의 일로 된 서울데이터'
+
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
